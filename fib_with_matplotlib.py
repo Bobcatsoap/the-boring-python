9b2a905e6446acc2e49d8fd143a4079aa7eaebd7
DESKTOP-FC3VJBG\BOBCATSOAP <bobcatsoap@gmail.com>
2018-06-10 00:03:12 +0800

    fib created using python turtle

---
+++ fib_with_matplotlib.py
@@ -0,0 +1,15 @@
+import turtle
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        myTurtle.circle(b * 2, 90)
+        a, b = b, a + b
+        n += 1
+    return "done"
+
+
+myTurtle = turtle.Turtle()
+fib(13)
+turtle.getscreen()._root.mainloop()
