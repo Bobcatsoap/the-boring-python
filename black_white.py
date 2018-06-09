c14647497c0d03c8d5e75b6b84fb066a31f17e85
DESKTOP-FC3VJBG\BOBCATSOAP <bobcatsoap@gmail.com>
2018-06-09 16:03:13 +0800

    OVER print,Path judgment,'jpeg' type

--- black_white.py
+++ black_white.py
@@ -1,14 +1,20 @@
 from PIL import Image
 import os
 
-dir_name = input("请输入文件夹名\n")
-real_path= dir_name + '/'
-file_list = os.listdir(real_path)
-extension_list = ['.png', '.jpg']
+dir_name = input("Please enter the folder name:\n")
+real_path = dir_name
 
-for file_name in file_list:
+if (not dir_name[len(dir_name)-1]=='/'):
+    real_path += '/'
+
+files_list = os.listdir(real_path)
+extension_list = ['.png', '.jpg', 'jpeg']
+
+for file_name in files_list:
     print(file_name)
     if (os.path.splitext(file_name)[1] in extension_list):
         image_file = Image.open(real_path + file_name)
         image_file = image_file.convert('L')
-        image_file.save(real_path+file_name + "black_white.png")
+        image_file.save(real_path + file_name + "_black_white.png")
+
+print("\nCONVERT OVER")
